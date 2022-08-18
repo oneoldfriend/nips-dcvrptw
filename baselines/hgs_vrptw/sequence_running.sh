1159d7809a5332c5b714930a45dd40d5fe520ad9
smu-leilinfei <121375919@qq.com>
2022-08-18 13:39:37 +0800

    hgs scripts running

--- baselines/hgs_vrptw/sequence_running.sh
+++ baselines/hgs_vrptw/sequence_running.sh
@@ -5,6 +5,6 @@ num_instances=`expr ${#instances[@]} - 1`
 
 for i in $(seq 0 6) # $num_instances)
 do
-    ./genvrp ${instances[$i]} 60 -seed 1 -veh -1 -useWallClockTime 1&
+    ./genvrp ${instances[$i]} 300 -seed 1 -veh -1 -useWallClockTime 1&
 done
 
