4aba937063df16b10dc7fb2c99bca8aafe0352e4
smu-leilinfei <121375919@qq.com>
2022-08-18 13:41:38 +0800

    paralel scripts for hgs

--- baselines/hgs_vrptw/sequence_running.sh
+++ baselines/hgs_vrptw/sequence_running.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+parallel_level=$1 # parallelism degree
 instances=( $(ls -1 ./instances/*) )
 num_instances=`expr ${#instances[@]} - 1`
+cur_paralel=1
 
-for i in $(seq 0 6) # $num_instances)
+for i in $(seq 0 $num_instances)
 do
-    ./genvrp ${instances[$i]} 300 -seed 1 -veh -1 -useWallClockTime 1&
+    ./genvrp ${instances[$i]} 300 -seed 1 -veh -1 -useWallClockTime 1 &
+    if [ "$cur_paralel" -gt "$parallel_level" ]; then
+    cur_paralel=1
+    wait
+  else
+    cur_paralel=$((cur_paralel + 1))
+  fi
 done
 
